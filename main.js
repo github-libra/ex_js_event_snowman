ba7fae82f4939f456d30f46ed74932b160da646d
tom <thj.web@icloud.com>
2016-03-05 12:36:59 +0800

    add mobile support

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 var which = '';
 var x = 0;
 var y = 0;
+
+function resetIndex() {
+    var imgs = document.querySelectorAll('img')
+    for (var i = imgs.length - 1; i >= 0; i--) {
+        imgs[i].style.zIndex = 5;
+    }
+}
 document.body.addEventListener('dragstart', function(e) {
     which = e.target;
     x = e.offsetX;
     y = e.offsetY;
+    resetIndex()
+    which.style.zIndex = 10;
 }, false)
 
 document.body.addEventListener('dragover', function(e) {
@@ -13,12 +22,21 @@ document.body.addEventListener('dragover', function(e) {
 
 document.body.addEventListener('drop', function(e) {
     e.preventDefault();
-    console.log('drag: ', which);
-    console.log('drop target', e.target);
-    console.log('drop position', e.pageX - x, e.pageY - y);
     which.style.left = e.pageX - x + 'px';
     which.style.top = e.pageY - y + 'px';
 }, false)
 
+document.body.addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    which = e.target;
+    var finger = e.touches[0];
+    var xx = which.offsetLeft - finger.pageX;
+    var yy = which.offsetTop - finger.pageY;
+    resetIndex()
+    which.style.zIndex = 10;
 
-
+    which.addEventListener('touchmove', function() {
+        which.style.left = finger.pageX + xx + 'px';
+        which.style.top = finger.pageY + yy + 'px';
+    }, false)
+}, false)
